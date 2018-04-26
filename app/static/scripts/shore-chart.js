1790dddd1f8e67d564234499a7494a8b3c911b65
Fedor Baart <fedor.baart@deltares.nl>
2018-04-26 22:29:55 +0200

    add line

--- app/static/scripts/shore-chart.js
+++ app/static/scripts/shore-chart.js
@@ -1,17 +1,18 @@
-function createShoreChart(json) {
+function createShoreChart(feature) {
 
   var elementId = 'chart-container' ;
 
   // data from global shore json file (single location)
-  var props = json.properties;
-  var intercept = props.intercept;
-  var dt = props.dt;
-  var dist = props.distances;
+  var properties = feature.properties;
+  var intercept = properties.intercept;
+  var dt = properties.dt;
+  var dist = properties.distances;
 
   var data = [];
   $.each(dist, function( index, value ) {
     var item = {};
     item.value = dist[index] - intercept;
+    item.dt = dt[index];
     item.date = new Date(1984 + dt[index], 0, 1);
     if (dist[index] === -999) {
       return;
@@ -21,8 +22,8 @@ function createShoreChart(json) {
 
   // D3js time series chart
   var margin = {top: 20, right: 20, bottom: 20, left: 40},
-    width = 600 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+      width = 600 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
 
   // Set the ranges
   var x = d3.time.scale().range([0, width]);
@@ -30,48 +31,38 @@ function createShoreChart(json) {
 
   // Define the axes
   var xAxis = d3.svg.axis().scale(x)
-    .orient("bottom").ticks(10);
+      .orient("bottom").ticks(10);
 
   var yAxis = d3.svg.axis().scale(y)
-    .orient("left").ticks(5);
+      .orient("left").ticks(5);
 
-  // Define the line
-  var valueline = d3.svg.line()
-    .x(function (d) {
-      return x((d.date));
-    })
-    .y(function (d) {
-      return y(d.value);
-    });
+  // Define the regression
+  var abLine = d3.svg.line()
+      .x(function (d) {
+        return x(d.date);
+      })
+      .y(function (d) {
+        console.log('dt', d.dt, d.dt * feature.properties.change_rate, feature);
+        return y(d.dt * feature.properties.change_rate);
+      });
 
   // clear content
   d3.select('#' + elementId)
     .selectAll('*')
     .remove();
 
-    // Adds the svg canvas
+  // Adds the svg canvas
   var svg = d3.select("#"+ elementId)
-    .append("svg")
-    .classed({'query-chart': true})
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform",
-    "translate(" + margin.left + "," + margin.top + ")");
-
-  // var tip = d3.tip()
-  //   .attr('class', 'd3-tip')
-  //   .offset([-10, 0])
-  //   .html(function (d) {
-  //     return 'Date: ' + formatDate(d.date) + ', Value: ' + d.value;
-  //   });
-
-  // svg.call(tip);
-
-  var lineSvg = svg.append("g");
+      .append("svg")
+      .classed({'query-chart': true})
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
 
   var focus = svg.append("g")
-    .style("display", "none");
+      .style("display", "none");
 
   // Scale the range of the data
   x.domain(d3.extent(data, function (d) {
@@ -81,15 +72,23 @@ function createShoreChart(json) {
     return d.value;
   }));
 
+  var lineGroup = svg.append("g");
+  lineGroup
+    .attr("class", "abline")
+    .append("path")
+    .attr('d', abLine(data));
+
+
   // Add the dotsh.
-    svg.append("g")
+  svg.append("g")
     .attr("class", "dots")
     .selectAll("path")
     .data(data)
-    .enter().append("path")
+    .enter()
+    .append("path")
     .attr("transform", function(d) { return "translate(" + x(d.date) + "," + y(d.value) + ")"; })
     .attr("d", d3.svg.symbol()
-    .size(40));
+          .size(40));
 
 
   // Add the X Axis
